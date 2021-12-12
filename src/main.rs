4648a09b8e064a9c43ad7d9cc6c20ad187c8a9ec
brianb <brianb@backblaze.com>
2021-12-12 21:51:25 +0000

    Correct answer for day 1a sample; code very kludgy

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 use std::env;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 
-fn main() {
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn day_1_a(lines: Box<dyn Iterator<Item = std::io::Result<String>>>) -> Result<String> {
+    let mut prev: Option<u64> = None;
+    let mut count: u64 = 0;
+    for line in lines {
+        let value: u64 = line?.parse()?;
+        let is_increase = match prev {
+            Some(prev_value) => prev_value < value,
+            None => false,
+        };
+        if is_increase {
+            count += 1;
+        }
+        prev = Some(value)
+    }
+    Ok(format!("{}", count))
+}
+
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!("Usage: advent <dayNumber>");
+        println!("Usage: advent <problemName>");
         std::process::exit(1);
     }
-    let day_number: i32 = args[1].parse().unwrap();
-    println!("Day {:?}", day_number);
+    let problem_name = &args[1];
+    let input_file = format!("input/{}/sample.txt", problem_name);
+    println!("Reading file: {}", input_file);
+    let file = File::open(input_file)?;
+    let input_lines = BufReader::new(file).lines();
+    let answer = day_1_a(Box::new(input_lines));
+    println!("answer: {}", answer?);
+    Ok(())
 }
