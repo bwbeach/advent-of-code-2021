06cbf36f7420d21ff9d31c4657daa1ca9f199c4a
brianb <brianb@backblaze.com>
2021-12-21 01:21:11 +0000

    more efficient: do all of the fish of the same age together

--- src/day6.rs
+++ src/day6.rs
@@ -1,27 +1,38 @@
 use crate::types::{AdventResult, Answer, Day, DayPart};
 
-type Counter = u8;
+/// Counter for the number of fish at a given age (countdown number)
+type FishCount = u64;
 
-fn parse_input(lines: &Vec<String>) -> Vec<Counter> {
+/// The state, holding the number of fish for each count-down value.
+type State = [FishCount; 9];
+
+fn parse_input(lines: &Vec<String>) -> State {
     if lines.len() != 1 {
         panic!("expected exactly one input line");
     }
-    lines[0].split(",").map(|s| s.parse().unwrap()).collect()
+    let counters: Vec<usize> = lines[0].split(",").map(|s| s.parse().unwrap()).collect();
+    let mut state: State = [0; 9];
+    for c in counters.iter() {
+        state[*c] += 1;
+    }
+    state
 }
 fn day_6_a(lines: &Vec<String>) -> AdventResult<Answer> {
     let mut state = parse_input(lines);
     for _ in 0..80 {
-        let initial_len = state.len();
-        for i in 0..initial_len {
-            if state[i] == 0 {
-                state[i] = 6;
-                state.push(8);
-            } else {
-                state[i] -= 1;
-            }
-        }
+        let zeros = state[0];
+        state[0] = state[1];
+        state[1] = state[2];
+        state[2] = state[3];
+        state[3] = state[4];
+        state[4] = state[5];
+        state[5] = state[6];
+        state[6] = state[7] + zeros;
+        state[7] = state[8];
+        state[8] = zeros;
     }
-    Ok(state.len() as u64)
+    let sum: u64 = state.iter().sum();
+    Ok(sum)
 }
 
 fn day_6_b(_lines: &Vec<String>) -> AdventResult<Answer> {
