a46f37d105b00577467a3428b8ed46062d8479a7
brianb <brianb@backblaze.com>
2021-12-24 23:32:55 +0000

    grid supports optional diagonal neighbors

--- src/grid.rs
+++ src/grid.rs
@@ -14,6 +14,9 @@ pub struct Neighbors {
     x: usize,
     y: usize,
 
+    // include diagonals?
+    include_diagonals: bool,
+
     // how many neighbors we've returned so far
     i: usize,
 }
@@ -21,34 +24,66 @@ pub struct Neighbors {
 impl Iterator for Neighbors {
     type Item = (usize, usize);
     fn next(&mut self) -> Option<(usize, usize)> {
+        let at_left = self.x == 0;
+        let at_right = self.x == self.width - 1;
+        let at_top = self.y == 0;
+        let at_bottom = self.y == self.height - 1;
         loop {
             self.i += 1;
             match self.i {
                 // the cell to the left
                 1 => {
-                    if self.x != 0 {
+                    if !at_left {
                         return Some((self.x - 1, self.y));
                     }
                 }
 
-                // the cell to the right
+                // diagonal: up and left
                 2 => {
-                    if self.x != self.width - 1 {
-                        return Some((self.x + 1, self.y));
+                    if self.include_diagonals && !at_left && !at_top {
+                        return Some((self.x - 1, self.y - 1));
                     }
                 }
 
                 // the cell above
                 3 => {
-                    if self.y != 0 {
+                    if !at_top {
                         return Some((self.x, self.y - 1));
                     }
                 }
 
-                // the cell below
+                // diagonal: up and right
                 4 => {
-                    if self.y != self.height - 1 {
+                    if self.include_diagonals && !at_right && !at_top {
+                        return Some((self.x + 1, self.y - 1));
+                    }
+                }
+
+                // the cell to the right
+                5 => {
+                    if !at_right {
+                        return Some((self.x + 1, self.y));
+                    }
+                }
+
+                // diagonal: down and right
+                6 => {
+                    if self.include_diagonals && !at_right && !at_bottom {
+                        return Some((self.x + 1, self.y + 1));
+                    }
+                }
+
+                // the cell below
+                7 => {
+                    if !at_bottom {
                         return Some((self.x, self.y + 1));
+                    }
+                }
+
+                // diagonal: down and right
+                8 => {
+                    if self.include_diagonals && !at_left && !at_bottom {
+                        return Some((self.x - 1, self.y + 1));
                     }
                 }
 
@@ -62,19 +97,51 @@ impl Iterator for Neighbors {
 }
 
 #[test]
-fn test_neighbors() {
+fn test_neighbors_no_diagonals() {
     fn run_one(width: usize, height: usize, x: usize, y: usize) -> Vec<(usize, usize)> {
         Neighbors {
             width,
             height,
             x,
             y,
+            include_diagonals: false,
             i: 0,
         }
         .collect()
     }
     assert_eq!(vec![(1, 0), (0, 1)], run_one(2, 2, 0, 0));
-    assert_eq!(vec![(0, 1), (2, 1), (1, 0), (1, 2)], run_one(3, 3, 1, 1));
+    assert_eq!(vec![(0, 1), (1, 0)], run_one(2, 2, 1, 1));
+    assert_eq!(vec![(0, 1), (1, 0), (2, 1), (1, 2)], run_one(3, 3, 1, 1));
+}
+
+#[test]
+fn test_neighbors_with_diagonals() {
+    fn run_one(width: usize, height: usize, x: usize, y: usize) -> Vec<(usize, usize)> {
+        Neighbors {
+            width,
+            height,
+            x,
+            y,
+            include_diagonals: true,
+            i: 0,
+        }
+        .collect()
+    }
+    assert_eq!(vec![(1, 0), (1, 1), (0, 1)], run_one(2, 2, 0, 0));
+    assert_eq!(vec![(0, 1), (0, 0), (1, 0)], run_one(2, 2, 1, 1));
+    assert_eq!(
+        vec![
+            (0, 1),
+            (0, 0),
+            (1, 0),
+            (2, 0),
+            (2, 1),
+            (2, 2),
+            (1, 2),
+            (0, 2)
+        ],
+        run_one(3, 3, 1, 1)
+    );
 }
 
 #[derive(PartialEq)]
@@ -100,6 +167,19 @@ impl Grid {
             height: shape[1],
             x: pos.0,
             y: pos.1,
+            include_diagonals: false,
+            i: 0,
+        }
+    }
+
+    pub fn neigbors_with_diagonals(&self, pos: (usize, usize)) -> Neighbors {
+        let shape = self.values.shape();
+        Neighbors {
+            width: shape[0],
+            height: shape[1],
+            x: pos.0,
+            y: pos.1,
+            include_diagonals: true,
             i: 0,
         }
     }
