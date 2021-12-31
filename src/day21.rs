ddca0f0b187fc17bf149b050ce96efb8ff5755a1
brianb <brianb@backblaze.com>
2021-12-31 00:25:37 +0000

    day 21 b

--- src/day21.rs
+++ src/day21.rs
@@ -1,3 +1,6 @@
+use std::cmp::max;
+use std::collections::HashMap;
+
 use crate::types::{AdventResult, Answer, Day, DayPart};
 
 /// Treats a range as a wraparound, and wraps
@@ -42,7 +45,7 @@ impl Die {
 }
 
 // Represents the current state of one player
-#[derive(Debug)]
+#[derive(Clone, Debug, Hash, Eq, PartialEq)]
 struct Player {
     // position is always in the range 1..=10
     position: usize,
@@ -72,7 +75,7 @@ impl Player {
 }
 
 /// The current state is the game.
-#[derive(Debug)]
+#[derive(Clone, Debug, Hash, Eq, PartialEq)]
 struct State {
     // the players
     players: [Player; 2],
@@ -87,6 +90,11 @@ impl State {
         let player = &mut self.players[self.next];
         self.next = 1 - self.next;
         player.one_move(roll)
+    }
+
+    // Is the game over?
+    fn game_over(&self, winning_score: usize) -> bool {
+        winning_score <= self.players[0].score || winning_score <= self.players[1].score
     }
 
     // The score that the loser had after somebody won.
@@ -111,14 +119,61 @@ fn day_21_a(lines: &[&str]) -> AdventResult<Answer> {
     Ok((state.loser_score() * die.count) as Answer)
 }
 
-fn day_21_b(_lines: &[&str]) -> AdventResult<Answer> {
-    Ok(0)
+fn day_21_b(lines: &[&str]) -> AdventResult<Answer> {
+    let winning_score = 21;
+    // the number of ways each total can come up using three three-side dice
+    let roll_and_count: [(usize, usize); 7] =
+        [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)];
+
+    // Mapping from state to the number of universes that have that state
+    // We start off with a single universe holding the initial state.
+    let mut state_to_universes: HashMap<State, usize> = HashMap::new();
+    state_to_universes.insert(parse_input(lines), 1);
+
+    // Run until all universes have complete games.
+    loop {
+        let mut new_state_to_universes = HashMap::new();
+        let mut all_complete = true;
+        for (state, universe_count) in state_to_universes {
+            if !state.game_over(winning_score) {
+                for (roll, count) in roll_and_count {
+                    let mut new_state = state.clone();
+                    let new_score = new_state.one_move(roll);
+                    (*new_state_to_universes.entry(new_state).or_insert(0)) +=
+                        universe_count * count;
+                    if new_score < winning_score {
+                        all_complete = false;
+                    }
+                }
+            } else {
+                (*new_state_to_universes.entry(state).or_insert(0)) += universe_count;
+            }
+        }
+        state_to_universes = new_state_to_universes;
+        if all_complete {
+            break;
+        }
+    }
+
+    // Count the wins for each player
+    let mut player_1_wins = 0;
+    let mut player_2_wins = 0;
+    for (state, universe_count) in state_to_universes {
+        if winning_score <= state.players[0].score {
+            player_1_wins += universe_count;
+        } else {
+            player_2_wins += universe_count;
+        }
+    }
+    println!("win counts: {:?} {:?}", player_1_wins, player_2_wins);
+
+    Ok(max(player_1_wins, player_2_wins) as Answer)
 }
 
 pub fn make_day_21() -> Day {
     Day::new(
         21,
         DayPart::new(day_21_a, 739785, 805932),
-        DayPart::new(day_21_b, 0, 0),
+        DayPart::new(day_21_b, 444356092776315, 133029050096658),
     )
 }
