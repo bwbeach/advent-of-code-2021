506a794e8f7b7ae2fa30e1c5054bd610b6825976
bwbeach <coder@beachfamily.net>
2022-01-08 21:49:09 +0000

    add FromStr and tests to RegisterName

--- src/day24_alu.rs
+++ src/day24_alu.rs
@@ -1,6 +1,16 @@
 use std::fmt;
+use std::str;
 
+/// Error type used for a variety of things in this day24_alu module.
+#[derive(Debug, Eq, PartialEq)]
+pub enum AluError {
+    BadRegisterName(String),
+}
+
+/// The name of a register in the ALU
+///
 /// Registers are named 'w' through 'z'
+///
 #[derive(Clone, Copy, Eq, PartialEq)]
 pub struct RegisterName {
     name: char,
@@ -20,7 +30,7 @@ impl RegisterName {
 
     pub fn parse(s: &str) -> RegisterName {
         if s.len() != 1 {
-            panic!("register name too long");
+            panic!("register name wrong length: {:?}", s);
         }
         RegisterName::new(s.chars().next().unwrap())
     }
@@ -34,4 +44,32 @@ impl fmt::Debug for RegisterName {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.name)
     }
+}
+
+impl str::FromStr for RegisterName {
+    type Err = AluError;
+
+    fn from_str(s: &str) -> Result<RegisterName, AluError> {
+        if s.len() != 1 {
+            return Err(AluError::BadRegisterName(s.to_string()));
+        }
+        let name = s.chars().next().unwrap();
+        if name < 'w' || 'z' < name {
+            return Err(AluError::BadRegisterName(s.to_string()));
+        }
+        Ok(RegisterName { name })
+    }
+}
+
+#[test]
+fn test_register_name() {
+    assert_eq!(
+        AluError::BadRegisterName("bad".to_string()),
+        "bad".parse::<RegisterName>().err().unwrap()
+    );
+    assert_eq!(
+        AluError::BadRegisterName("m".to_string()),
+        "m".parse::<RegisterName>().err().unwrap()
+    );
+    assert_eq!(RegisterName::new('x'), "x".parse::<RegisterName>().unwrap());
 }
